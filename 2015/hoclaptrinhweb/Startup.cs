e5b365661f8efcb8b105e63b877e1514395214b0
vonhatnam100689@gmail.com <vonhatnam100689@gmail.com@5f72b32d-728c-4aff-0aa2-1793758e7c04>
2014-11-02 11:19:46 +0000

    lan dau

---
+++ 2015/hoclaptrinhweb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(hoclaptrinhweb.Startup))]
+namespace hoclaptrinhweb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
